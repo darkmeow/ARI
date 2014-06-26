79cca68c2d83769e4fd16ea1bef32c0a48672ee9
Barbarita Lara <blara@alumnos.inf.utfsm.cl>
2014-06-25 22:30:53 -0300

    todo

--- tarea3/consultas.sql
+++ tarea3/consultas.sql
@@ -6,32 +6,37 @@ SELECT CONCAT (nombre, apellido_paterno, apellido_materno), carrera
 FROM 
   public."Empleados"
 WHERE 
-  carrera = 'Ingeniería Civil Informática' ;
+  carrera = 'Ingeniería Civil Informática'
+;
 
 --Mostrar el nombre y el stock de todos los productos que hay en kiosko
 
 SELECT nombre,stock
 FROM 
-  public."Productos" ;
+  public."Productos"
+;
 
 --Mostrar el nombre completo de los empleados que trabajan los "Viernes"
 
 SELECT CONCAT(A1.nombre,A1.apellido_paterno,A1.apellido_materno) NOMBRE, A2.rut RUT
 FROM public."Empleados" A1, public."Horarios" A2
-WHERE A1.rut = A2.rut AND A2.dia = 'Viernes';
+WHERE A1.rut = A2.rut AND A2.dia = 'Viernes'
+;
 
 --Mostrar el nombre de todos los productos que cuestan menos de $500
 
 SELECT nombre, stock
 FROM
   public."Productos"
-WHERE stock < 500;
+WHERE stock < 500
+;
 
 --Mostrar el nombre completo de todos los empleados que hayan vendido algún "Queque" un "Martes"
 
-SELECT *,to_char(fecha_y_hora::timestamp, 'TMDay') AS dia
-FROM public."Ventas"
-GROUP BY id_producto, rut, fecha_y_hora
+SELECT CONCAT(A1.nombre, A1.apellido_paterno, A1.apellido_materno) NOMBRE,to_char(fecha_y_hora::timestamp, 'TMDay') AS dia
+FROM public."Ventas" Ventas, public."Empleados" A1
+GROUP BY Ventas.rut, fecha_y_hora, nombre, apellido_paterno, apellido_materno, A1.rut
+WHERE A1.rut = Ventas.rut 
 HAVING to_char(fecha_y_hora::timestamp, 'TMDay') = 'Martes' 
 ;
 
